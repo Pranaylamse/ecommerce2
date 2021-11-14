94e730b582a4d0180b7c5f2e478bd980ca56526b
pranay lamse <pranaylamsegood@gmail.com>
2021-11-14 21:57:47 +0530

    commit

---
+++ precache-manifest.b7d28477ab47e89c2d9ac29195136ea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3a679e01625788658283ece3ef963e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0052aaeaee1ff3fb730",
+    "url": "/static/js/2.10deb5b7.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.10deb5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d253c6b142f414b3208",
+    "url": "/static/js/main.022bfb42.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
